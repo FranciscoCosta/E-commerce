5d895b8e05f2b6a7d96ac094e9f27efbe993b676
Francisco Costa <francisco100eg@gmail.com>
2022-09-13 22:53:05 -0300

    Header.jsx responsivo

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from "../images/logo.png";
+import { FaCartArrowDown } from 'react-icons/fa';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className='header-container'>
+        <div className='header-logo'>
+            <img src={logo} alt='Trybe Store logo'></img>
+        </div>
+        <div className='header-mid'>
+        <input
+            type="text"
+            className="header-input"
+          />
+          <button
+            type="button"
+            className="header-btn"
+          >
+            Buscar
+          </button>
+        </div>
+        <FaCartArrowDown size={30} color="#2fc18c" className='header-cart'/>
+      </header>
+    )
+  }
+}
